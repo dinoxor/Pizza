c75112bc5dd47f77e69e6b53bacaa2f4e9b6cd9e
Quang Mai <q.mai@wearedoubleline.com>
2018-04-05 13:04:33 -0500

    Add attributes

--- The Ace of Spades Pizza/Attributes/MaxNumberOfDaysAttribute.cs
+++ The Ace of Spades Pizza/Attributes/MaxNumberOfDaysAttribute.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
 namespace The_Ace_of_Spades_Pizza.Attributes
 {
-    public class CurrentDateAttribute: ValidationAttribute
+    public class MaxNumberOfDaysAttribute : ValidationAttribute
     {
+        private readonly DateTime _maxDate;
+
+        public MaxNumberOfDaysAttribute(int maxDays)
+        {
+            _maxDate = DateTime.Now.AddDays(maxDays);
+        }
+
         public override bool IsValid(object value)
         {
             var datetime = (DateTime) value;
-            if (datetime >= DateTime.Now)
+            if (datetime <= _maxDate)
             {
                 return true;
             }
